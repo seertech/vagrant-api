a606cacc141580cd0530354824793138c1a0397b
Joseph Ilagan <joben.ilagan@gmail.com>
2014-08-31 13:43:04 +0800

    modified bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,19 @@ export DEBIAN_FRONTEND=noninteractive
 # Install Apache2
 apt-get -y install apache2
 
+if [ ! -h /var/www ]; 
+then 
+
+    rm -rf /var/www sudo 
+    ln -s /vagrant/public /var/www
+
+    a2enmod rewrite
+
+    sed -i '/AllowOverride None/c AllowOverride All' /etc/apache2/sites-available/default
+
+    service apache2 restart
+fi
+
 # Install MongoDB
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
